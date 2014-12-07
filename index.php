a5c1691346051030fd4cbfcd3e64f6dd5efbbce6
lennonj <lennonj@indiana.edu>
2014-12-07 13:42:39 -0500

    minor mods to text on index and contact info

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 	<h4 style="text-align:left;">Microbial Biodiversity: Evolution To Ecosystems</h4>
 </div>        
 <p>
-Microorganisms are the most abundant and diverse life forms on Earth. They attain high population densities, have fast reproductive rates, and evolve rapidly to changes in their environment. Moreover, microbes carry out important functions, including nutrient cycling, trace gas flux, and carbon sequestration, which are important for the stability of natural and managed ecosystems. 
+Microorganisms are the most abundant and diverse life forms on Earth. They attain high population densities, have fast reproductive rates, and evolve rapidly to changes in their environment. Moreover, microbes carry out important processes that are important for the stability of natural and managed ecosystems. 
 </p>
 
-<p>We study the ecology and evolution of microbial communities. We are interested in the biotic and abiotic factors that generate and maintain microbial biodiversity. In turn, we seek to understand the implications of microbial diversity for ecosystem functioning. We conduct research in terrestrial and aquatic habitats, and use a variety of tools including molecular biology, simulation modeling, laboratory experiments, field surveys, and whole ecosystem manipulations in natural and managed ecosystems. </p>
+<p>We study the ecology and evolution of microbial communities. We are interested in the biotic and abiotic factors that generate and maintain microbial biodiversity. In turn, we seek to understand the implications of microbial diversity for ecosystem functioning. Our research uses various tools including molecular biology, simulation modeling, laboratory experiments, field surveys, and whole ecosystem manipulations in a wide range of haibtats. </p>
 
 <p>
 Visit the <a href="http://www.indiana.edu/~lennon/index.php?title=Main_Page">Lab Wiki</a> for news and additional information
@@ -23,7 +23,7 @@ Follow the Lennon Lab on <a href="https://twitter.com/jaytlennon">Twitter</a>
 
 <h4 style="text-align:left;">Contact:</h4>
 <p>
-<a href="http://www.indiana.edu/~microbes/people.php/cfm#Lennon"> Jay T. Lennon</a>, <a href="http://www.bio.indiana.edu/">  Department of Biology</a>, <a href="http://www.iub.edu/">Indiana University</a>, 261 Jordan Hall, <a href="https://www.google.com/maps/place/Indiana+University/@39.169135,-86.514905,17z/data=!3m1!4b1!4m2!3m1!1s0x886c66c26789ad33:0x3499a08bb315d436">1001 East 3rd Street</a>, <a href="http://bloomington.in.gov/">Bloomington, IN</a> 47405 USA, 812-856-0962 (office), 812-856-7235 (lab), 855-6082 (fax), Email: <a href="mailto:lennonj@indiana.edu">lennonj@indiana.edu</a>
+<a href="http://www.indiana.edu/~microbes/people.php/cfm#Lennon"> Jay T. Lennon</a>, <a href="http://www.bio.indiana.edu/">  Department of Biology</a>, <a href="http://www.iub.edu/">Indiana University</a>, 261 Jordan Hall, <a href="https://www.google.com/maps/place/Indiana+University/@39.169135,-86.514905,17z/data=!3m1!4b1!4m2!3m1!1s0x886c66c26789ad33:0x3499a08bb315d436">1001 East 3rd Street</a>, <a href="http://bloomington.in.gov/">Bloomington, IN</a> 47405 USA, 812-856-0962 (office), 812-856-7235 (lab), <a href="mailto:lennonj@indiana.edu">lennonj@indiana.edu</a>
 </p>
 
 </div>
