62604cf178b15b71e2e40b59cda0cf4c9a475acc
lennonj <lennonj@indiana.edu>
2020-03-25 15:44:16 -0400

    updated pubs, people, etc.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 Microorganisms are the most abundant and diverse life forms on Earth. They have fast reproductive rates and evolve rapidly to changes in their environment. Microbes also carry out processes that are critical for the stability of engineered, environmental, and host-associated ecosystems.
 </p>
 
-<p>We study the ecology and evolution of microbial communities. We are interested in the factors that generate and maintain microbial biodiversity. In turn, we seek to understand the implications of microbial diversity for ecosystem functioning. To address these questions, our team uses a combination of molecular biology, modeling, data mining and synthesis, laboratory experiments, and field surveys in a wide range of habitats. </p>
+<p>From test tubes to the globe, we study the ecology and evolution of microbial communities. We are interested in the factors that generate and maintain microbial biodiversity. In turn, we seek to understand the implications of microbial diversity for ecosystem functioning. To address these questions, our team uses a combination of molecular biology, modeling, data mining and synthesis, laboratory experiments, and field surveys in a wide range of habitats. </p>
 
 <p>
 For additional information and news, visit the <a href="https://lennon.sitehost.iu.edu">Lab Wiki</a> and follow us on <a href="https://twitter.com/jaytlennon">Twitter</a>. Data and code for some of our projects can be found on <a href="https://github.com/LennonLab">GitHub</a>. 
