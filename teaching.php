5ef7eefe52570bc85157bb1f334185e076f482a4
Jay Lennon <lennonj@indiana.edu>
2022-07-21 00:18:21 -0400

    udpate people and pubs

--- teaching.php
+++ teaching.php
@@ -25,7 +25,7 @@
 </div>
 
 <div id="right-side-research">
-<h5>Quantitative Biodiversity </h5>
+<h5>Quantitative biodiversity </h5>
 <p><b>BIOL Z620</b></p>
 <p>
 In this graduate-level course we focus on theory, patterns, metrics, and tools used to study biodiversity. Students calculate diversity metrics, generate and quantify diversity relationships, visualize multivariate data, and conduct phylogenetic tests. Students use modern statistical computing and graphics environments (i.e., <a href=" http://www.r-project.org/"> R</a>), as well as version control tools (i.e., <a href="https://github.com/QuantitativeBiodiversity"> GitHub</a>). See <a href="http://documentup.com/QuantitativeBiodiversity/QuantitativeBiodiversity"> course website</a> and the <a href="http://www.mendeley.com/groups/5054771/z620-quantitative-biodiversity/">open-access bibliography</a> for more information.
@@ -33,6 +33,23 @@ In this graduate-level course we focus on theory, patterns, metrics, and tools u
 </div>
 
 <div style="clear: both;"><br /><br /></div>
+	
+<!--- entry --->
+<div id="left-side">
+<img src="https://microbes.sitehost.iu.edu/images/teaching/colormicro.jpg" />
+</div>
+
+<div id="right-side-research">
+<h5>Microbiomes: host and environmental health  </h5>
+<p><b>BIOL L472 </b></p>
+<p>
+In this <a href="https://microbes.sitehost.iu.edu/teaching/L472_Advert.pdf">upper-level course</a>, we cover fundamental concepts of ecology & evolutionary biology as they pertain to to microbial systems. In addition to lectures and discussion of papers from the primary literature, students engage in biodiversity projects that result in oral presentations and a contribution to <a href="https://microbewiki.kenyon.edu/index.php/MicrobeWiki">MicrobeWiki</a>, a open resource on microbes and microbiology. Course offered in spring semesters. See course <a href="https://microbes.sitehost.iu.edu/teaching/L472_Syllabus.pdf">syllabus</a> and <a href="https://microbes.sitehost.iu.edu/teaching/L472_Schedule.pdf">schedule</a>.
+</p>
+</div>
+
+<div style="clear: both;"><br /><br /></div>	
+	
+	
 <!--- entry --->
 <div id="left-side">
 <img src="https://microbes.sitehost.iu.edu/images/teaching/microbiomes.jpg" />
@@ -49,20 +66,6 @@ In this graduate-level class, we focus on the microbiomes of different hosts (hu
 
 <div style="clear: both;"><br /><br /></div>
 
-<!--- entry --->
-<div id="left-side">
-<img src="https://microbes.sitehost.iu.edu/images/teaching/colormicro.jpg" />
-</div>
-
-<div id="right-side-research">
-<h5>Microbial Ecology  </h5>
-<p><b>BIOL L472 </b></p>
-<p>
-In this <a href="https://microbes.sitehost.iu.edu/teaching/L472_Advert.pdf">upper-level course</a>, we cover fundamental concepts of ecology & evolutionary biology as they pertain to to microbial systems. In addition to lectures and discussion of papers from the primary literature, students engage in biodiversity projects that result in oral presentations and a contribution to <a href="https://microbewiki.kenyon.edu/index.php/MicrobeWiki">MicrobeWiki</a>, a open resource on microbes and microbiology. Course offered in spring semesters. See course <a href="https://microbes.sitehost.iu.edu/teaching/L472_Syllabus.pdf">syllabus</a> and <a href="https://microbes.sitehost.iu.edu/teaching/L472_Schedule.pdf">schedule</a>.
-</p>
-</div>
-
-<div style="clear: both;"><br /><br /></div>
 
 </div>
 
